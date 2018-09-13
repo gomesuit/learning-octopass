9855b07815c94a6f53e09a0e97e1869c6077bf93
gomesuit <9404411+gomesuit@users.noreply.github.com>
2018-09-14 07:42:18 +0900

    save

--- install-common-package.sh
+++ install-common-package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
 curl -s https://packagecloud.io/install/repositories/linyows/octopass/script.rpm.sh | bash
 yum install -y octopass
-curl https://raw.githubusercontent.com/linyows/octopass/master/octopass.conf.example -sS -o /etc/octopass.conf
+# curl https://raw.githubusercontent.com/linyows/octopass/master/octopass.conf.example -sS -o /etc/octopass.conf
+cp /vagrant/octopass.conf /etc/octopass.conf
