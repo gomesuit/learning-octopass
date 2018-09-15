2d92679661392240869d8a487330d31815a42ef3
gomesuit <9404411+gomesuit@users.noreply.github.com>
2018-09-15 18:04:07 +0900

    save

--- install-common-package.sh
+++ install-common-package.sh
@@ -5,11 +5,11 @@ yum install -y vim
 curl -s https://packagecloud.io/install/repositories/linyows/octopass/script.rpm.sh | bash
 yum install -y octopass
 # curl https://raw.githubusercontent.com/linyows/octopass/master/octopass.conf.example -sS -o /etc/octopass.conf
-cp /vagrant/settings/etc/octopass.conf /etc/octopass.conf
+ln -fs /vagrant/settings/etc/octopass.conf /etc/octopass.conf
 
-cp /vagrant/settings/etc/ssh/sshd_config /etc/ssh/sshd_config
-cp /vagrant/settings/etc/pam.d/sshd /etc/pam.d/sshd
-cp /vagrant/settings/etc/pam.d/system-auth-ac /etc/pam.d/system-auth-ac
-cp /vagrant/settings/etc/nsswitch.conf /etc/nsswitch.conf
+ln -fs /vagrant/settings/etc/ssh/sshd_config /etc/ssh/sshd_config
+ln -fs /vagrant/settings/etc/pam.d/sshd /etc/pam.d/sshd
+ln -fs /vagrant/settings/etc/pam.d/system-auth-ac /etc/pam.d/system-auth-ac
+ln -fs /vagrant/settings/etc/nsswitch.conf /etc/nsswitch.conf
 
 systemctl reload sshd
